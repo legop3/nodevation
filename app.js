bd7517560a1e225844fef630164e5b69a13e6244
legop3 <minecratfd8@gmail.com>
2022-04-06 01:57:31 -0400

    wokig on it

--- app.js
+++ app.js
@@ -1,24 +1,61 @@
+const { get } = require('http');
 const Launchpad = require( 'launchpad-mini' ),
 pad = new Launchpad();
-pad.flash = true
+var cron = require('node-cron')
+// pad.flash = true
+
+
+// const setbuttons = [
+//     [{
+//         x:6,
+//         y:6,
+//         command:'http://localhost:6969'
+//     }]
+// ]
+// console.log(setbuttons[0].y)
+
+
 pad.on( 'connect', () => console.log( 'Launchpad connected!' ) );
 pad.connect().then( () => {     // Auto-detect Launchpad
+    // pad.reset(3)
     pad.reset(0)
+    pad.setColors([
+        [6,6,pad.red],
+        [6,7,pad.red]
+    ]);
 
 
-    //set colors here, colors are red green amber off yellow
-    pad.col(pad.green.medium, [0,5])
+
+
+
+    
 
 
 
     pad.on( 'key', k => {
+        console.log(pad.pressedButtons)
         console.log( `Key ${k.x},${k.y} down: ${k.pressed}`);
-
-        if(pad.isPressed([0,5])) {
-            console.log('chrome')
+        // pad.col(pad.green, pad.pressedButtons)
+        if(pad.isPressed([6,6])) {
+            console.log('mute')
+            get("http://localhost:6969/mute")
         }
 
+        if(pad.isPressed([6,7])){
+            console.log('deafen')
+            get("http://localhost:6969/deafen")
+        } else {
+            
+        }
 
     } );
+
+
+
+
+
+
+
+
 } );
 
